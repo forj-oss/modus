f574ed4a6e655e27bf8b3a9268296e5774a01837
Diego De Nova <diegodnova@hp.com>
2014-04-21 19:41:03 -0500

    User story #1425: Create module for installing bugzilla in modus blueprint
    Change-Id: I32712a16420dc046696b0b5d7ba1c52c9cf3e9b5

--- puppet/modules/bugzilla/manifests/init.pp
+++ puppet/modules/bugzilla/manifests/init.pp
@@ -1,10 +1,52 @@
-	# this module triggers all the scripts needed for Bugzilla's set up
+  # (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+  #
+  #   Licensed under the Apache License, Version 2.0 (the "License");
+  #   you may not use this file except in compliance with the License.
+  #   You may obtain a copy of the License at
+  #
+  #       http://www.apache.org/licenses/LICENSE-2.0
+  #
+  #   Unless required by applicable law or agreed to in writing, software
+  #   distributed under the License is distributed on an "AS IS" BASIS,
+  #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  #   See the License for the specific language governing permissions and
+  #   limitations under the License.
 
-	# class definition - start
-	class bugzilla {
-		#include conditions
-		include mysql_add_user
-		include apache_add_vhost
-		include bugzilla_setup
-	}
-	# class definition - end
+  # this module triggers all the scripts needed to install bugzilla
+
+  # class definition - start
+  class bugzilla {
+
+    # ensures that the directory for bugzilla to be installed is present
+    if ! defined(File['/usr/local/bugzilla']) {
+      file { '/usr/local/bugzilla':
+        ensure   =>   directory,
+        owner    =>   root,
+        group    =>   www-data,
+      }
+    }
+
+    # classes to be instantiated
+    include bugzilla::perl_package
+    include bugzilla::mysql_server_package
+    include bugzilla::httpd_package
+    include bugzilla::git_package
+
+    class { 'bugzilla::bugzilla_package':
+      require   =>   File['/usr/local/bugzilla'],
+    }
+    class { 'bugzilla::perl_modules_package':
+      option    =>   'required',
+      require   =>   Class['bugzilla::bugzilla_package'],
+    }
+    class { 'bugzilla::mysql_server_config':
+      require   =>   Class['bugzilla::perl_modules_package'],
+    }
+    class { 'bugzilla::bugzilla_config':
+      require   =>   Class['bugzilla::mysql_server_config'],
+    }
+    class { 'bugzilla::httpd_config':
+      require   =>   Class['bugzilla::bugzilla_config'],
+    }
+  }
+  # class definition - end
